81fe0260b23289c84d3e85d086b48617f3802fcf
Ibad <ibad23ahmad@gmail.com>
2021-03-14 16:20:04 +0500

    adding abuzar projects

---
+++ portfolio/src/components/hero-text.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import "./main.css";
+import ReactTypingEffect from "react-typing-effect";
+
+function HeroText() {
+  const TypingStyle = {
+    fontWeight: "bold",
+    letterSpacing: "0.3rem",
+    color: "maroon",
+  };
+  return (
+    <div id="home">
+      <div className="Hero-text">
+        <div className="Big-text">
+          More Of <br />
+          <span>an agency</span>
+        </div>
+        <div className="Typing-text">
+          <div>
+            We are software company and we are providing
+            <div>
+              <ReactTypingEffect
+                text={[" Web Development.", " Web Designing."]}
+                cursorRenderer={(cursor) => <h3>{cursor}</h3>}
+                typingDelay="300"
+                eraseDelay="3000"
+                displayTextRenderer={(text, i) => {
+                  return (
+                    <div>
+                      {text.split("").map((char, i) => {
+                        const key = `${i}`;
+                        return (
+                          <span key={key} style={TypingStyle}>
+                            {char}
+                          </span>
+                        );
+                      })}
+                    </div>
+                  );
+                }}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default HeroText;
---
+++ portfolio/src/components/nav.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './main.css';
+
+function Nav() {
+    return (
+        <div className="nav-links">
+            <div className="logo">
+                <img src="logo.png" alt="Logo"/>
+            </div>
+        </div>
+    )
+}
+
+export default Nav;
